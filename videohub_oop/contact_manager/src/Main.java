16b91d1878c6eec6b2bc546052bf198108ff2414
Bobur <boburotaboyev0@gmail.com>
2025-11-17 00:02:33 +0500

    some changed

--- videohub_oop/contact_manager/src/Main.java
+++ videohub_oop/contact_manager/src/Main.java
@@ -4,126 +4,13 @@ import static java.lang.System.exit;
 
 public class Main {
 
-    static Contact[] contactArr = new Contact[2];
-    static int currenIndex = 0;
 
     public static void main(String[] args) {
 
-
-        while (true) {
-            menu();
-            int n = getAction();
-
-            switch (n) {
-                case 1:
-                    Contact contact = addContact();
-                    addToArray(contact);
-                    break;
-                case 2:
-                    printContactList();
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 0:
-                    exit(0);
-                    break;
-                default:
-                    System.out.println("Please choose current number");
-                    break;
-
-            }
-        }
-    }
-
-    public static void menu() {
-        System.out.println("** Menu **");
-        System.out.println("1.Add Contact");
-        System.out.println("2.Contact List");
-        System.out.println("3.Search");
-        System.out.println("4.Delete contact");
-        System.out.println("0.Exit");
-    }
-
-    public static int getAction() {
-        System.out.print("Enter your choice: ");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
-    }
-
-    public static Contact addContact() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.next();
-
-        System.out.print("Enter your surname: ");
-        String surname = scanner.next();
-
-        System.out.print("Enter your phone: ");
-        String phone = scanner.next();
-
-
-        Contact contact = new Contact();
-        contact.name = name;
-        contact.surname = surname;
-        contact.phone = phone;
-
-        return contact;
+        ContactManager contactManager = new ContactManager();
+        contactManager.start();
 
     }
 
-    public static boolean isValidContact(Contact contact) {
-
-        if (contact.name == null || contact.name.trim().length() < 3) {
-            System.out.println("Name is wrong");
-            return false;
-        }
-
-        if (contact.surname == null || contact.surname.trim().length() < 3) {
-            System.out.println("Surname is wrong");
-            return false;
-        }
-
-        if (contact.phone == null || contact.phone.trim().length() < 7) {
-            System.out.println("Phone is wrong");
-            return false;
-        }
-
-        char[] phoneArr = contact.phone.toCharArray();
-        for (char ch : phoneArr) {
-            if (!Character.isDigit(ch)) {
-                System.out.println("Phone number is wrong");
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    public static void addToArray(Contact contact) {
-        if (isValidContact(contact)) {
-            if (currenIndex == contactArr.length) {
-                Contact[] newArr = new Contact[contactArr.length + 1];
-                for (int i = 0; i < contactArr.length; i++) {
-                    newArr[i] = contactArr[i];
-
-                }
-                contactArr = newArr;
-            }
-            contactArr[currenIndex] = contact;
-            currenIndex++;
-            System.out.println(contact.name + " " + contact.surname);
-        }
-    }
-
-    public static void printContactList() {
-        for (Contact c : contactArr) {
-            if (c == null) {
-                break;
-            }
-            System.out.println(c.name + " " + c.surname);
-        }
-    }
 
 }
